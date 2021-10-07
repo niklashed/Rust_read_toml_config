48cd4e2cc12f50c1e70bfb48cda0b411107bcf60
Niklas Hedlund <nhed@pop-os.localdomain>
2021-10-07 16:19:51 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 //! An example showing off the usage of `Deserialize` to automatically decode
 //! TOML into a Rust `struct`
 
-#![deny(warnings)]
-
 use serde_derive::Deserialize;
 use std::fs::File;
 use std::io::prelude::*;
 
-/// This is what we're going to decode into. Each field is optional, meaning
-/// that it doesn't have to be present in TOML.
 #[derive(Debug, Deserialize)]
 struct Config {
     global_string: Option<String>,
@@ -16,10 +12,6 @@ struct Config {
     lin_ports: Vec<LinConfig>,
 }
 
-/// Sub-structs are decoded from tables, so this will decode from the `[server]`
-/// table.
-///
-/// Again, each field is optional, meaning they don't have to be present.
 #[derive(Debug, Deserialize)]
 struct LinConfig {
     uart: String,
